5544f9422c1213d12de4607b0ffe9244c9231612
ashu180674 <ashutripathi16.10.2001@gmail.com>
2024-02-21 10:15:01 +0530

    jkl

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [url, seturl] = useState("")
+  const [start, setstart] = useState(0);
+  const [load, setload] = useState(0);
+
+  const submit = async () => {
+    if (url !== "") {
+
+
+      // const data=axios.post("65.0.184.177:5000/deploy",{"github_link":url})
+      setload(1);
+      setTimeout(() => {
+        setstart(1);
+        setload(0);
+      }, 40000);
+      seturl("");
+
+    }
+    else {
+      alert("Please enter the github link")
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div class="container">
+        {
+          load == 1 && <div>
+            Loading....
+          </div>
+        }
+        {
+          load == 0 &&
+          <>
+            <h1>Paste Your GitHub URL</h1>
+            <div>
+              {start === 1 && <a href='http://sparcel.tech.s3-website.ap-south-1.amazonaws.com/'>click to open site</a>}
+              {start == 0 && <input value={url} onChange={e => seturl(e.target.value)} type="text" id="github_link" name="github_link" placeholder="Enter your GitHub URL" required />}
+              <button onClick={submit}>Submit</button>
+            </div>
+          </>
+        }
+
+      </div>
+    </body>
   );
 }
 
